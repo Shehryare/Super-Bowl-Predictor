90d6a81cf3b3269ea7038a47e5b557a8b699405b
Shehryar Ehtesham <se318@rutgers.edu>
2020-07-19 00:29:50 -0400

    dataset size function made

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
+
+#Each set of lines should be ran in the main.pynb file using jupiter notebook. 
+#In this way you can view the visualizations 
+
+#
+#
+#
+#
+#
+#
+#
+
+#imports from path to find any datasets inside of your data folder
 from pathlib import Path
 data_dir_path = Path('data')
 file_names = [x.name for x in data_dir_path.glob('*') if x.is_file()]
+
+
+#prints the sizes of both datasets
+import os
+print('---Size of each dataset---\n')
+print('Super Bowl History:')
+print(os.path.getsize('data/datasets_superbowl.csv'))
+print('\nSuper Bowl Ads:')
+print(os.path.getsize('data/datasets_superbowl-ads.csv'))
+
+
